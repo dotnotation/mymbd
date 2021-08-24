d223a8692de89e63e44b8c22de16cf249c5d29ec
dotnotation <dotinmotionproductions@gmail.com>
2021-08-24 16:56:37 -0400

    DONE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_180004) do
+ActiveRecord::Schema.define(version: 2021_08_24_204943) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_08_24_180004) do
     t.date "released_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_08_24_180004) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "movies", "users"
 end
