497bce5696355416b554c5f2c54ef9854a8310d1
dotnotation <dotinmotionproductions@gmail.com>
2021-08-24 16:20:49 -0400

    added Faker and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
+  get '/movies/new', to: "movies#new"
+  
+  post '/movies', to: "movies#create"
+
   resources :movies
+  #[new, create, edit, update, delete, index, show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+count = 0
+users = 10
+movies = 200
+puts "Seeding User Data"
+puts "-----------------"
+users.times do
+    system('clear')
+    puts "seeding User Data"
+    puts "-----------------"
+    count += 1
+    puts "#{count/users.to_f*100}%"
+    password = Faker::Internet.password
+    user = User.new(username: Faker::Internet.username, password: password)
+    user.save
+end
+
+count = 0
+
+movies.times do 
+    system('clear')
+    puts "Seeding Movie Data"
+    puts "-----------------"
+    count += 1
+    puts "#{count/movies.to_f*100}%"
+    Movie.create(title: Faker::Movie.title, description: Faker::Lorem.paragraph_by_chars(number: 255), released_date: (0...365).to_a.sample.days.ago)
+end
+
+puts "------------"
+puts "User Data: 100%"
+puts "Movie Data: 100%"
+puts "Data Successfully Seeded!"
+puts "-------------"
